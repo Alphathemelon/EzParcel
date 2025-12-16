21d8ed8af1220d8eb4e4b6c7887cf0e99d5c3171
Aliff <a203272@siswa.ukm.my>
2025-12-16 22:48:42 +0800

    pembetulan

--- reportindex.php
+++ reportindex.php
@@ -3,83 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/stylereport.css">
+    <link rel="stylesheet" href="stylereport.css">
     <title>Report EzParcel</title>
 </head>
 <body>
-<<<<<<< HEAD
     
-    <div class="parent">
-
-        <div class="div1">
-            1
-        </div>
-
-        <div class="div2">
-            2
-        </div>
-
-        <div class="div3">
-            3
-        </div>
-
-        <div class="div4">
-            <canvas id="myChart1"></canvas>
-            <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-            <script>
-            const ctx1 = document.getElementById('myChart1');
-
-            new Chart(ctx1, {
-                type: 'bar',
-                data: {
-                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-                datasets: [{
-                    label: '# of Votes',
-                    data: [12, 19, 3, 5, 2, 3],
-                    borderWidth: 1
-                }]
-                },
-                options: {
-                scales: {
-                    y: {
-                    beginAtZero: true
-                    }
-                }
-                }
-            });
-            </script>
-        </div>
-
-        <div class="div5">
-            <canvas id="myChart"></canvas>
-            <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-            <script>
-            const ctx = document.getElementById('myChart');
-
-            new Chart(ctx, {
-                type: 'bar',
-                data: {
-                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-                datasets: [{
-                    label: '# of Votes',
-                    data: [12, 19, 3, 5, 2, 3],
-                    borderWidth: 1
-                }]
-                },
-                options: {
-                scales: {
-                    y: {
-                    beginAtZero: true
-                    }
-                }
-                }
-            });
-            </script>
-        </div>
-
-    </div>
-    
-=======
     <h1>Report EzParcel</h1>
     <div>
         <canvas id="myChart"></canvas>
@@ -89,6 +17,6 @@
 
 
     <script src="js/report.js"></script>
->>>>>>> 7635cb16e15676fa944f2b1974c491744258213b
+
 </body>
 </html>
